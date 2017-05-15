0c13a83780f8a4d2157c14820afe777ec419fdca
stephenliu <stephenlyao@yeah.net>
2017-05-15 12:03:05 +0800

    rm

--- webpack/middleware/webpack-hot.js
+++
@@ -1,6 +0,0 @@
-import koaWebpackHotMiddleware from 'koa-webpack-hot-middleware'
-import convert from 'koa-convert'
-
-export default function (compiler) {
-  return convert(koaWebpackHotMiddleware(compiler))
-}
