2ae02d7fadbf68bdd773616a5a661420e538594e
stephenliu <stephenlyao@yeah.net>
2017-08-04 19:08:31 +0800

    add config

--- src/FakeAMD.js
+++ src/FakeAMD.js
@@ -2,23 +2,27 @@
 //  Author by StephenLiu
 //  
 
-(function(window) {
+(function(root) {
   class FakeAMD { 
     constructor(root) {
       root.define = this.define.bind(this)
       root.require = this.require.bind(this)
+      root.require.config = this.requireConfig.bind(this)
     }
 
-    
-    define() {
-      console.log('define function')
+    define(name, deps, factory) {
+      
     }
 
     require() {
       console.log('require function')
     }
+
+    requireConfig() {
+      console.log('require config function')
+    }
   }
   // 实例化FakeAMD类，传入window
-  let fakeAmd = new FakeAMD(window)
+  let fakeAmd = new FakeAMD(root)
 })(window)
 
